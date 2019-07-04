d0840288c12a1f0c12fa8f2a7ef91fe9c7411f3a
Wiola Kilimnik <wiolettakilimnik@gmail.com>
2019-07-04 16:43:14 +0200

    initial styles to SingleDay added

--- src/Components/PlanDietView/SingleDay/MealSpace.js
+++ src/Components/PlanDietView/SingleDay/MealSpace.js
@@ -3,8 +3,9 @@ import s from './SingleDay.module.css'
 
 export function MealSpace (props) {
     let { mealType } = props
-    return <div className={s.MealSpace}>
+    return <div className={s.mealSpaceContainer}>
         <h3 className={s.mealTypeTitle}>{mealType}</h3>
+        <div className={s.mealSpace}></div>
     </div>
 }
 
--- src/Components/PlanDietView/SingleDay/Summary.js
+++ src/Components/PlanDietView/SingleDay/Summary.js
@@ -5,15 +5,15 @@ import s from './SingleDay.module.css'
 export function Summary(props) {
     let { sumCalories, kcalGoal, leftCalories } = props
     leftCalories = kcalGoal - sumCalories
-    
+
     return <div className={s.summaryContainer}>
         <div>
-            <h3>calories / kcal goal:</h3>
-            <h3>{sumCalories} / {kcalGoal}</h3>
+            <h3 className={s.summaryText}>calories / kcal goal:</h3>
+            <h3 className={s.summaryText}>{sumCalories} / {kcalGoal}</h3>
         </div>
         <div>
-            <h3>calories left:</h3>
-            <h3>{leftCalories}</h3>
+            <h3 className={s.summaryText}>calories left:</h3>
+            <h3 className={s.summaryText}>{leftCalories}</h3>
         </div>
     </div>
 }
