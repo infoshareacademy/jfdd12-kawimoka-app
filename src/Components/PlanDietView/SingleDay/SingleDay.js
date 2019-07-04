fc227ba671a5c8acdfef87cfa38357b7e232dc25
Wiola Kilimnik <wiolettakilimnik@gmail.com>
2019-07-04 15:41:41 +0200

    SingleDay module css created

--- src/Components/PlanDietView/SingleDay/SingleDay.js
+++ src/Components/PlanDietView/SingleDay/SingleDay.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { MealSpace } from './MealSpace'
 import { Summary } from './Summary'
+import s from './SingleDay.module.css'
 
 export const SingleDay = () => {
-    return <div>
+    return <div className={s.singleDayContainer}>
         <MealSpace mealType = {"Breakfast"} />
         <MealSpace mealType = {"Lunch"}/>
         <MealSpace mealType = {"Snacks"}/>
