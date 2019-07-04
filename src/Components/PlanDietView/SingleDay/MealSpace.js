130cc29c0ba30548c9146fcfc8cc63e004e05925
Wiola Kilimnik <wiolettakilimnik@gmail.com>
2019-07-04 15:49:16 +0200

    flex display added

--- src/Components/PlanDietView/SingleDay/MealSpace.js
+++ src/Components/PlanDietView/SingleDay/MealSpace.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import s from './SingleDay.module.css'
 
 export function MealSpace (props) {
     let { mealType } = props
-    return <div>
+    return <div className={s.MealSpace}>
         <h3>{mealType}</h3>
     </div>
 }
