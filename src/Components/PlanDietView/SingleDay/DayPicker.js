5f30e6689d750fecde7f47424762389e48382eec
Wioletta Kilimnik <wiolettakilimnik@gmail.com>
2019-07-07 18:42:08 +0200

    daypicker with decrement function added

--- src/Components/PlanDietView/SingleDay/DayPicker.js
+++ src/Components/PlanDietView/SingleDay/DayPicker.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import s from './SingleDay.module.css'
 
-export function DayPicker (props) {
+export class DayPicker extends React.Component {
 
-    let { day } = props
+    constructor(props){
+        super(props);
+        this.state = {
+            dateInner: props.date
+        }
+        this.decrementDate = this.decrementDate.bind(this);
+    }
 
-    return <div className={s.dayPicker}>
-        <button>{"<"}</button>
-        <h2>{day}</h2>
-        <button>{">"}</button>
-    </div>
+    decrementDate() {
+        this.setState((prevState) => ({
+            dateInner : prevState.dateInner.subtract('days', 1)
+        }));
+    }
+
+
+    render () {
+        return (
+            <div className={s.dayPicker}>
+                <button onClick={this.decrementDate}>{"<"}</button>
+                <h2>{this.state.dateInner.format("D-MMMM")}</h2>
+                <button>{">"}</button>
+            </div>)
+    }
 }
--- src/Components/PlanDietView/SingleDay/SingleDay.js
+++ src/Components/PlanDietView/SingleDay/SingleDay.js
@@ -3,16 +3,26 @@ import { MealSpace } from './MealSpace'
 import { SummaryDay } from './SummaryDay'
 import { DayPicker } from './DayPicker'
 import s from './SingleDay.module.css'
+import moment from 'moment';
 
-const SingleDay = () => {
-    return <div className={s.singleDayContainer}>
-    <DayPicker day = {15} />
-        <MealSpace mealType = {"Breakfast"} />
-        <MealSpace mealType = {"Lunch"}/>
-        <MealSpace mealType = {"Snacks"}/>
-        <MealSpace mealType = {"Dinner"}/>
-        <SummaryDay sumCalories = {"1230"} kcalGoal = {"1800"} leftCalories />
-    </div>
+class SingleDay extends React.Component {
+    state = {
+        date: moment()
+    }
+
+
+    render () {
+        return (
+            <div className={s.singleDayContainer}>
+            <DayPicker date = {this.state.date} />
+                <MealSpace mealType = {"Breakfast"} />
+                <MealSpace mealType = {"Lunch"}/>
+                <MealSpace mealType = {"Snacks"}/>
+                <MealSpace mealType = {"Dinner"}/>
+                <SummaryDay sumCalories = {"1230"} kcalGoal = {"1800"} leftCalories />
+            </div>
+            )
+        }
 }
 
 export default SingleDay
