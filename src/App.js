d530cf5f91a5bca6987d3a0df2229e24f3b0e22b
Monika Gapinska <gapinska.mon@gmail.com>
2019-07-08 15:31:01 +0200

    selecting dates connected with singleday view

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { PlanDietContainer } from './Components/PlanDietView/PlanDietContainer'
-import moment from 'moment';
+import React from "react";
+import { PlanDietContainer } from "./Components/PlanDietView/PlanDietContainer";
+import moment from "moment";
+import Calendar from "./Components/Calendar/Calendar";
 
 class App extends React.Component {
-
   state = {
     date: moment()
-  }
+  };
 
-  render () {
+  render() {
+    console.log(this.state.date);
     return (
-      <PlanDietContainer date = {this.state.date} />
+      <>
+        <PlanDietContainer date={this.state.date} />
+
+        <Calendar
+          selectedDate={this.state.date}
+          setSelectedDate={date => this.setState({ date })}
+        />
+      </>
     );
   }
-  
 }
 
 export default App;
