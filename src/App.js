9f93dae868f0c5d5a75c35a19f500052de39d3de
Wiola Kilimnik <wiolettakilimnik@gmail.com>
2019-07-08 17:00:30 +0200

    meals added next to singleday

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { PlanDietContainer } from "./Components/PlanDietView/PlanDietContainer";
 import moment from "moment";
-import Calendar from "./Components/Calendar/Calendar";
+import Calendar from "./Components/Calendar/Calendar"
+import { MealCardShort } from './Components/meal/MealCardShort'
+import { MealsList } from './Components/meal/MealsList';
 
 class App extends React.Component {
   state = {
@@ -11,14 +13,15 @@ class App extends React.Component {
   render() {
     console.log(this.state.date);
     return (
-      <>
+      <div style={{display: "flex", flexDirection: "row"}}>
+        <MealsList />
         <PlanDietContainer date={this.state.date} />
 
-        <Calendar
+        {/* <Calendar
           selectedDate={this.state.date}
           setSelectedDate={date => this.setState({ date })}
-        />
-      </>
+        /> */}
+      </div>
     );
   }
 }
