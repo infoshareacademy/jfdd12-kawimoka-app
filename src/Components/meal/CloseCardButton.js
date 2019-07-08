9b27a019b4097f47f77ff9e07c17e3f96cd25bff
Katarzyna Pastwa <kasia.pastwa@gmail.com>
2019-07-08 16:44:24 +0200

    cleannig staff

--- src/Components/meal/CloseCardButton.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import styles from './Meal.module.css';
-
-export const CloseCardButton = () => (
-  <button onClick={() => onMealClose() } className={styles.closeCardButton}> X </button>
-);
-
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { MealsList } from './components/meal/MealsList';
+import { MealsList } from './Components/meal/MealsList';
 
 
 ReactDOM.render(<MealsList />, document.getElementById('root'));
