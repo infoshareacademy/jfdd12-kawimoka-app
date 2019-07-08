82075bafa9fabac8b9a8a7d9c567c3a8d5e3ff2c
Wiola Kilimnik <wiolettakilimnik@gmail.com>
2019-07-08 16:30:17 +0200

    kcal added to mealspace

--- src/Components/PlanDietView/SingleDay/MealSpace.js
+++ src/Components/PlanDietView/SingleDay/MealSpace.js
@@ -2,7 +2,8 @@ import React from 'react'
 import s from './SingleDay.module.css'
 
 export function MealSpace (props) {
-    let { mealType } = props
+    let { mealType, kcal } = props
+    
     return <div className={s.mealSpaceContainer}>
         <h3 className={s.mealTypeTitle}>{mealType}</h3>
         <div className={s.mealSpace}></div>
