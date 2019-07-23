73c1ac6f1a9be14f8c67cdcd5d068ea06f45eb17
Wioletta Kilimnik <wiolettakilimnik@gmail.com>
2019-07-23 20:13:08 +0200

    counting macros function refactor

--- src/Components/PlanDietView/Nutrition/Nutrition.js
+++ src/Components/PlanDietView/Nutrition/Nutrition.js
@@ -14,18 +14,11 @@ export function Nutrition() {
     { name: "Protein", value: sumNutrition("protein") || 1 }
   ];
 
-  function getMacrosValue(macroName) {
-    const macro = data.find(macro => {
-      return macro.name === macroName;
-    });
-    const macroValue = macro.value;
-    return macroValue === 1 ? 0 : macroValue;
-  }
-
   return (
-    <div className="nutritionContainer">
+    
+      
+      <Paper zDepth={3} className="nutritionContainer">
       <h2>Nutrition</h2>
-      <Paper zDepth={3} className="nutritionCard">
         <SummaryDay sumNutrition={sumNutrition} kcalGoal={"1800"} />
         <div>
           <NutritionsPieChart sumNutrition={sumNutrition} data={data} />
@@ -40,18 +33,18 @@ export function Nutrition() {
         >
           <li className="nutritionItem">
             <div className="squareFat" />
-            <h5>Fat: {getMacrosValue("Fat")}</h5>
+            <h5>Fat: {sumNutrition("fat") || 0} g</h5>
           </li>
           <li className="nutritionItem">
             <div className="squareCarbs" />
-            <h5>Carbs: {getMacrosValue("Carbs")}</h5>
+            <h5>Carbs: {sumNutrition("carbs") || 0} g</h5>
           </li>
           <li className="nutritionItem">
             <div className="squareProtein" />
-            <h5>Protein: {getMacrosValue("Protein")}</h5>
+            <h5>Protein: {sumNutrition("protein") || 0} g</h5>
           </li>
         </ul>
       </Paper>
-    </div>
+    
   );
 }
