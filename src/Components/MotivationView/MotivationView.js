7f490c34ce057325a1fd6784c53ac5eefd603576
Monika Gapinska <gapinska.mon@gmail.com>
2019-07-25 16:35:05 +0200

    RWD img MotivationView

--- src/Components/MotivationView/MotivationView.js
+++ src/Components/MotivationView/MotivationView.js
@@ -8,7 +8,7 @@ export class MotivationView extends React.Component {
 
   render() {
     return (
-      <Paper  zDepth={3} className="MotivationView" style={{maxHeight: "850px",marginTop: "20px", flexBasis: "450px", marginLeft: "25px"}}>
+      <Paper  zDepth={3} className="MotivationView" >
        <img src="https://i.pinimg.com/originals/0f/e2/c4/0fe2c428b941241572482b8efbf78543.jpg" style={{maxHeight: "850px"}}/>
       </Paper >
     );
