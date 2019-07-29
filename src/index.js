e3bc71756d4f9e61902b4d80eff84d14ce0988c3
Monika Gapinska <gapinska.mon@gmail.com>
2019-07-29 12:54:43 +0200

    RWD finished

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import 'semantic-ui-css/semantic.min.css'
-import { AuthProvider } from './contexts/AuthContext'
-import { UserProvider } from './contexts/UserContext'
-import './firebase'
-import App from './App'
-import { PlanProvider } from './contexts/PlanContext'
-import 'semantic-ui-css/semantic.min.css'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "semantic-ui-css/semantic.min.css";
+import { AuthProvider } from "./contexts/AuthContext";
+import { UserProvider } from "./contexts/UserContext";
+import "./firebase";
+import App from "./App";
+import { PlanProvider } from "./contexts/PlanContext";
+import "semantic-ui-css/semantic.min.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./Components/Calendar/custom-calendar.css";
 
 ReactDOM.render(
   <AuthProvider>
@@ -18,5 +19,5 @@ ReactDOM.render(
       </PlanProvider>
     </UserProvider>
   </AuthProvider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
